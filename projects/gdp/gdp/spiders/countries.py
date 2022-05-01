f1e608b0580bb3cfcf6c19e59ab6787514519e51
Dajebbar <dajebbar@gmx.fr>
2022-05-01 17:05:53 +0000

    gdp

--- projects/gdp/gdp/spiders/countries.py
+++ projects/gdp/gdp/spiders/countries.py
@@ -4,7 +4,13 @@ import scrapy
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
     allowed_domains = ['worldpopulationreview.com']
-    start_urls = ['http://worldpopulationreview.com/']
+    start_urls = ['http://worldpopulationreview.com/countries/countries-by-national-debt']
 
     def parse(self, response):
-        pass
+        countries = response.xpath("//tbody/tr")
+        for country in countries:
+            yield {
+                'country_name': country.xpath(".//td[1]/a/text()").get(),
+                'debt_to_gdp_ratio': country.xpath(".//td[2]/text()").get(),
+                'population': country.xpath(".//td[3]/text()").get(),
+            }
