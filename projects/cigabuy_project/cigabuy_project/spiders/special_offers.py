610574c64539983e3093539bb4f29897375af4b0
Dajebbar <dajebbar@gmx.fr>
2022-05-03 11:51:09 +0000

    promo special products

--- projects/cigabuy_project/cigabuy_project/spiders/special_offers.py
+++ projects/cigabuy_project/cigabuy_project/spiders/special_offers.py
@@ -7,28 +7,22 @@ class SpecialOffersSpider(scrapy.Spider):
     start_urls = ['https://www.cigabuy.com/specials.html']
 
     def parse(self, response):
-        wrapers = response.xpath("//div[@class='p_box_wrapper']/div")
-        stars = response.xpath("//div[@class='p_box_star']")
-        prices = response.xpath("//div[@class='p_box_price cf']")
+        wrapers = response.xpath("//ul[@class='productlisting-ul']/div[@class='p_box_wrapper']")
+        
         for wraper in wrapers:
-            product_title = wraper.xpath(".//a[@class='p_box_title']/text()").get()
-            product_link = wraper.xpath(".//a[@class='p_box_img']/@href").get()
-            product_img = wraper.xpath(".//a[@class='p_box_img']/img/@data-original").get()
+            product_title = wraper.xpath(".//div/a[@class='p_box_title']/text()").get()
+            product_link = wraper.xpath(".//div/a[@class='p_box_img']/@href").get()
+            product_img = wraper.xpath(".//div/a[@class='p_box_img']/img/@data-original").get()
+            product_stars = wraper.xpath(".//div[@class='p_box_star']/a/@href").get()
+            original_price = wraper.xpath("//div[@class='p_box_price cf']/span[@class='normalprice fl']/text()").get()
+            promo_price = wraper.xpath("//div[@class='p_box_price cf']/span[@class='productSpecialPrice fl']/text()").get()
 
-            yield{
-                'product_name': product_title,
-                'link': product_link,
-                'product_img': product_img,
-            }
-        for star in stars:
-            product_stars = star.xpath(".//a/@href").get()
-
-            yield{
-                'product_stars': product_stars,
-            }
-        for price in prices:
-            product_price = price.xpath(".//text()").get()
-            
             yield {
-                'product_price': product_price,
-                }
+                'product_title': product_title,
+                'product_link': product_link, 
+                'product_img': product_img,
+                'product_stars': product_stars,
+                'original_price': original_price,
+                'promo_price': promo_price
+            }
+               
