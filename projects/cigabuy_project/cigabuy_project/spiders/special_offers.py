0dbce933f3c45ba70de2e9112895425df6f31e28
Dajebbar <dajebbar@gmx.fr>
2022-05-03 12:09:57 +0000

    modified div

--- projects/cigabuy_project/cigabuy_project/spiders/special_offers.py
+++ projects/cigabuy_project/cigabuy_project/spiders/special_offers.py
@@ -7,15 +7,15 @@ class SpecialOffersSpider(scrapy.Spider):
     start_urls = ['https://www.cigabuy.com/specials.html']
 
     def parse(self, response):
-        wrapers = response.xpath("//ul[@class='productlisting-ul']/div[@class='p_box_wrapper']")
+        wrapers = response.xpath("//ul[@class='productlisting-ul']/div[@class='p_box_wrapper']/div")
         
         for wraper in wrapers:
-            product_title = wraper.xpath(".//div/a[@class='p_box_title']/text()").get()
-            product_link = wraper.xpath(".//div/a[@class='p_box_img']/@href").get()
-            product_img = wraper.xpath(".//div/a[@class='p_box_img']/img/@data-original").get()
-            product_stars = wraper.xpath(".//div/div[@class='p_box_star']/a/@href").get()
-            original_price = wraper.xpath(".//div/div[@class='p_box_price cf']/span[@class='normalprice fl']/text()").get()
-            promo_price = wraper.xpath(".//div/div[@class='p_box_price cf']/span[@class='productSpecialPrice fl']/text()").get()
+            product_title = wraper.xpath(".//a[@class='p_box_title']/text()").get()
+            product_link = wraper.xpath(".//a[@class='p_box_img']/@href").get()
+            product_img = wraper.xpath(".//a[@class='p_box_img']/img/@data-original").get()
+            product_stars = wraper.xpath(".//div[@class='p_box_star']/a/@href").get()
+            original_price = wraper.xpath(".//div[@class='p_box_price cf']/span[@class='normalprice fl']/text()").get()
+            promo_price = wraper.xpath(".//div[@class='p_box_price cf']/span[@class='productSpecialPrice fl']/text()").get()
 
             yield {
                 'product_title': product_title,
