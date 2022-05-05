b1dccbe2ef5b25a8efe8adafb1fa0333bc6491e4
Dajebbar <dajebbar@gmx.fr>
2022-05-05 14:33:15 +0000

    crawl multiple pages and set user-agent

--- projects/book_to_scrap_project/book_to_scrap_project/spiders/books.py
+++ projects/book_to_scrap_project/book_to_scrap_project/spiders/books.py
@@ -6,14 +6,33 @@ from scrapy.spiders import CrawlSpider, Rule
 class BooksSpider(CrawlSpider):
     name = 'books'
     allowed_domains = ['books.toscrape.com']
-    start_urls = ['http://books.toscrape.com/']
+    # start_urls = ['http://books.toscrape.com/']
+
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36'
+
+    def start_requests(self):
+        yield scrapy.Request(
+            url='http://books.toscrape.com/',
+            headers={
+                'User-Agent': self.user_agent
+            }
+        )
 
     rules = (
         Rule(LinkExtractor(restrict_xpaths="//div[@class='image_container']/a"), 
         callback='parse_item', 
-        follow=True
+        follow=True, 
+        process_request='set_user_agent',
+        ),
+
+        Rule(LinkExtractor(restrict_xpaths="//li[@class='next']/a"), 
+        process_request='set_user_agent',
         ),
     )
+
+    def set_user_agent(self, request, response):
+        request.headers['User-Agent'] = self.user_agent
+        return request
 
     def parse_item(self, response):
         yield {
