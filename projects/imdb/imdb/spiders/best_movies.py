9ecaefe9b8f8b02e7499359608d169c313d435ee
Dajebbar <dajebbar@gmx.fr>
2022-05-05 12:57:30 +0000

    scraping 1000 best imdb movies

--- projects/imdb/imdb/spiders/best_movies.py
+++ projects/imdb/imdb/spiders/best_movies.py
@@ -6,10 +6,11 @@ from scrapy.spiders import CrawlSpider, Rule
 class BestMoviesSpider(CrawlSpider):
     name = 'best_movies'
     allowed_domains = ['www.imdb.com']
-    start_urls = ['https://www.imdb.com/search/title/?genres=drama&groups=top_250&sort=user_rating,desc']
+    start_urls = ['https://www.imdb.com/search/title/?groups=top_1000']
 
     rules = (
         Rule(LinkExtractor(restrict_xpaths="//h3[@class='lister-item-header']/a"), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="(//a[@class='lister-page-next next-page'])[2]"))
     )
 
     def parse_item(self, response):
