bbc6586070c1eddaddba01615a906aa505ae89b8
Dajebbar <dajebbar@gmx.fr>
2022-05-05 12:25:20 +0000

    crawl template

--- projects/imdb/imdb/spiders/best_movies.py
+++ projects/imdb/imdb/spiders/best_movies.py
@@ -13,4 +13,16 @@ class BestMoviesSpider(CrawlSpider):
     )
 
     def parse_item(self, response):
-       print(response.url)
+    #    print(response.url)
+        yield {
+            'title': response.xpath("//div[@class='sc-94726ce4-1 iNShGo']/h1/text()").get(),
+            'year': response.xpath("(//span[@class='sc-8c396aa2-2 itZqyK'])[1]/text()").get(),
+            'genre': response.xpath("(//li[@class='ipc-inline-list__item ipc-chip__text'])[1]/text()").get(),
+            'public_permission': response.xpath("(//span[@class='sc-8c396aa2-2 itZqyK'])[2]/text()").get(),
+            'duration_hr': response.xpath("(//li[@class='ipc-inline-list__item'])[3]/text()[1]").get(),
+            'duration_min': response.xpath("(//li[@class='ipc-inline-list__item'])[3]/text()[4]").get(),
+            'noted_imdb': response.xpath("//span[@class='sc-7ab21ed2-1 jGRxWM']/text()").get(),
+            'movie_poster': response.urljoin(response.xpath("//a[@class='ipc-lockup-overlay ipc-focusable']/@href").get()),
+            'play_trailer': response.urljoin(response.xpath("//a[@class='ipc-lockup-overlay sc-5ea2f380-2 gdvnDB hero-media__slate-overlay ipc-focusable']/@href").get()),
+            'movie_url': response.url,
+        }
