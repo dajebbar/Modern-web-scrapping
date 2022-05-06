f9b6fdc2eb11e180887c9237191d8ca6d9a5fa16
Dajebbar <dajebbar@gmx.fr>
2022-05-06 13:57:36 +0000

    add lua script call splashreq

--- projects/livecoin/livecoin/spiders/coin.py
+++ projects/livecoin/livecoin/spiders/coin.py
@@ -1,10 +1,38 @@
 import scrapy
-
+from scrapy_splash import SplashRequest
 
 class CoinSpider(scrapy.Spider):
     name = 'coin'
-    allowed_domains = ['www.livecoin.net']
-    start_urls = ['http://www.livecoin.net/']
+    allowed_domains = ['www.livecoin.net/en']
+    # start_urls = ['http://www.livecoin.net/']
+
+    script = '''
+                function main(splash, args)
+                splash.private_mode_enable: false
+                
+                url = args.url
+                assert(splash:go(url))
+                assert(splash:wait(1))
+                
+                rur_tab = assert(splash:select_all(".filterPanelItem__2z5Gb"))
+                rur_tab[5] = mouse_click()
+                assert(splash:wait(1))
+                splash:set_viewport_full()
+                return {
+                    html = splash:html(),
+                    png = splash:png(),
+                }
+                end
+            '''
+    def start_requests(self):
+        yield SplashRequest(
+            url='http://www.livecoin.net/en',
+            callback=self.parse,
+            endpoint='execute',
+            args={
+                'lua_source': self.script,
+            }
+        ) 
 
     def parse(self, response):
-        pass
+       print(response.body)
