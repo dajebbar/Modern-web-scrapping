d39620c1a0719f112ecf6a503e142b1275834aa6
Dajebbar <dajebbar@gmx.fr>
2022-05-07 09:57:35 +0000

    codes

--- projects/quotes_project/quotes_project/spiders/quotes.py
+++ projects/quotes_project/quotes_project/spiders/quotes.py
@@ -9,40 +9,30 @@ class QuotesSpider(scrapy.Spider):
     # start_urls = ['http://quotes.toscrape.com/']
 
     script = '''
-                    function main(splash, args)
-                        assert(splash:go(args.url))
-                        assert(splash:wait(0.5))
-                        
-                        quotes = assert(splash:select_all(".quotes"))
-                        assert(splash:wait(0.5))
-                        return {
-                            html = splash:html(),
-                            png = splash:png(),
-                        }
-                    end
-            '''
-    
+        function main(splash, args)
+            assert(splash:go(args.url))
+            assert(splash:wait(0.5))
+            return splash:html()
+        end
+    '''
+
     def start_requests(self):
-        yield SplashRequest(
-            url='http://quotes.toscrape.com/',
-            callback=self.parse,
-            endpoint='execute',
-            args={
-                'lua_source': self.script,
-            }
-        )
+        yield SplashRequest(url='http://quotes.toscrape.com/js/', callback=self.parse, endpoint='execute', args={
+            'lua_source': self.script
+        })
 
     def parse(self, response):
-        for quote in response.xpath("//div[@class='quote']"):
+        quotes = response.xpath("//div[@class='quote']")
+        for quote in quotes:
             yield {
-                'text': quote.xpath(".//span[1]/text()").get(),
-                'author': quote.xpath(".//span[2]/small/text()").get(),
-                'tags': quote.xpath(".//div/a/text()").get(),
+                'text': quote.xpath(".//span[@class='text']/text()").get(),
+                'author': quote.xpath(".//span[2]/small[@class='author']/text()").get(),
+                'tags': quote.xpath(".//div[@class='tags']/a/text()").getall()
             }
-        
-        next_page = response.xpath("//li[@class='next']/a").get()
+
+        next_page = response.xpath("//li[@class='next']/a/@href").get()
         if next_page:
-            yield scrapy.Request(
-                url=next_page,
-                callback=self.parse,
-            )
+            absolute_url = f"http://quotes.toscrape.com{next_page}"
+            yield SplashRequest(url=absolute_url, callback=self.parse, endpoint='execute', args={
+                'lua_source': self.script
+            })
