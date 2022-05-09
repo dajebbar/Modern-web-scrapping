3c62fba0201b6dbd20cb54168f784dfa37d69f9a
Dajebbar <dajebbar@gmx.fr>
2022-05-09 09:33:13 +0100

    scrap the first page

--- projects/demo_api/demo_api/spiders/quotes_api.py
+++ projects/demo_api/demo_api/spiders/quotes_api.py
@@ -10,5 +10,12 @@ class QuotesApiSpider(scrapy.Spider):
     def parse(self, response):
         resp = json.loads(response.body)
         quotes = resp.get('quotes')
-        print(quotes)
+        # print(quotes)
         # print(response.body)
+
+        for quote in quotes:
+            yield {
+                'author': quote.get('author').get('name'),
+                'tags': quote.get('tags'),
+                'quote_text': quote.get('text'),
+            }
