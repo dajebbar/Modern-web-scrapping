c12e08ede3d790815e660acffd21575a30ff820f
Dajebbar <dajebbar@gmx.fr>
2022-05-09 07:24:05 +0100

    access not allowed

--- projects/coinMarketCap/coinMarketCap/spiders/coin_market.py
+++ projects/coinMarketCap/coinMarketCap/spiders/coin_market.py
@@ -6,15 +6,14 @@ from scrapy.spiders import CrawlSpider, Rule
 class CoinMarketSpider(CrawlSpider):
     name = 'coin_market'
     allowed_domains = ['coinmarketcap.com']
-    start_urls = ['http://coinmarketcap.com/']
+    start_urls = ['https://coinmarketcap.com/']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="//div[@class='sc-16r8icm-0 escjiH']/a[@class='cmc-link']"), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        yield {
+            'name': response.xpath("//h2[@class='sc-1q9q90x-0 jCInrl h1']/text()").get(),
+            'price': response.xpath("//div[@class='priceValue ']/span/text()").get(),
+        }
