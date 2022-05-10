5291ce6996905e06d6a0e9e37dccdf581dd14c00
Dajebbar <dajebbar@gmx.fr>
2022-05-10 08:03:55 +0100

    FromRequest to login

--- projects/demo_login/demo_login/spiders/quote_login.py
+++ projects/demo_login/demo_login/spiders/quote_login.py
@@ -1,10 +1,25 @@
 import scrapy
+from scrapy import FormRequest
 
 
 class QuoteLoginSpider(scrapy.Spider):
     name = 'quote_login'
     allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+    start_urls = ['https://quotes.toscrape.com/login']
 
     def parse(self, response):
-        pass
+        csrf_token = response.xpath("//input[@name='csrf_token']/@value").get()
+        yield FormRequest.from_response(
+            response,
+            formxpath="//form",
+            formdata={
+                'csrf_token': csrf_token,
+                'user': 'admin',
+                'password': 'admin',
+            },
+            callback=self.after_login,
+        )
+    
+    def after_login(self, response):
+        if response.xpath("//a[@href='/logout']/text()").get():
+            print('Logged In')
