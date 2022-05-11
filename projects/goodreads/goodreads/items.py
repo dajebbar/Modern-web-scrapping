82034f99ab1c6ae0505be2c844445c2c276568b7
Dajebbar <dajebbar@gmx.fr>
2022-05-11 10:34:03 +0100

    items clean process

--- projects/goodreads/goodreads/items.py
+++ projects/goodreads/goodreads/items.py
@@ -4,12 +4,40 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import (
+    MapCompose,
+    TakeFirst,
+    Join,
+)
+from w3lib.html import remove_tags
 
-
+def remove_quotations(value):
+    return value.replace(u"\u201d", '').replace(u"\u201c", '')
 class GoodreadsItem(scrapy.Item):
     # Create Fields
     author_img = scrapy.Field()
-    text = scrapy.Field()
-    author_name = scrapy.Field()
-    tags = scrapy.Field()
-    likes = scrapy.Field()
+    text = scrapy.Field(
+        input_processor=MapCompose(
+            str.strip,
+            remove_quotations
+        ),
+        output_processor=TakeFirst()
+    )
+    author_name = scrapy.Field(
+        input_processor=MapCompose(
+            remove_tags
+        ),
+        output_processor=TakeFirst()
+    )
+    tags = scrapy.Field(
+        input_processor=MapCompose(
+            remove_tags
+        ),
+        output_processor=Join()
+    )
+    likes = scrapy.Field(
+        input_processor=MapCompose(
+            remove_tags
+        ),
+        output_processor=TakeFirst()
+    )
