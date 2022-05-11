cde322f21d875497204a224673cf423df635f0f8
Dajebbar <dajebbar@gmx.fr>
2022-05-11 08:33:32 +0100

    page one

--- projects/goodreads/goodreads/spiders/quote.py
+++ projects/goodreads/goodreads/spiders/quote.py
@@ -4,7 +4,17 @@ import scrapy
 class QuoteSpider(scrapy.Spider):
     name = 'quote'
     allowed_domains = ['www.goodreads.com']
-    start_urls = ['https://www.goodreads.com/']
+    start_urls = ['https://www.goodreads.com/quotes']
 
     def parse(self, response):
-        pass
+        quotes = response.xpath("//div[@class='quoteDetails']")
+        for quote in quotes:
+            yield {
+                'author_img': quote.xpath(".//a[@class='leftAlignedImage']/img/@src").get(),
+                'text': quote.xpath("normalize-space(.//div[@class='quoteText']/text())").get(),
+                'author_name': quote.xpath("normalize-space(.//span[@class='authorOrTitle']/text())").get(),
+                'tags': quote.xpath("//div[@class='greyText smallText left']/a/text()").getall(),
+                'likes': quote.xpath("//a[@class='smallText']/text()").get(),
+            }
+
+        
