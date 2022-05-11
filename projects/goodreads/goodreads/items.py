e7a24bca959cd12b92f510920a2aeebc4c000e23
Dajebbar <dajebbar@gmx.fr>
2022-05-11 10:47:49 +0100

    add more clean func

--- projects/goodreads/goodreads/items.py
+++ projects/goodreads/goodreads/items.py
@@ -13,19 +13,37 @@ from w3lib.html import remove_tags
 
 def remove_quotations(value):
     return value.replace(u"\u201d", '').replace(u"\u201c", '')
+
+def clean_authors(value):
+    if ',' in value:
+        return value[:-1]
+    return value
+
+def likes(value):
+    if " " in value:
+        return value[:value.index(" ")]
+    return value
+
+def clean_text(value):
+    if '―' in value:
+        return value[:value.index('―')]
+    return value
+
 class GoodreadsItem(scrapy.Item):
     # Create Fields
     author_img = scrapy.Field()
     text = scrapy.Field(
         input_processor=MapCompose(
             str.strip,
-            remove_quotations
+            remove_quotations,
+            clean_text
         ),
         output_processor=TakeFirst()
     )
     author_name = scrapy.Field(
         input_processor=MapCompose(
-            remove_tags
+            remove_tags,
+            clean_authors
         ),
         output_processor=TakeFirst()
     )
@@ -37,7 +55,8 @@ class GoodreadsItem(scrapy.Item):
     )
     likes = scrapy.Field(
         input_processor=MapCompose(
-            remove_tags
+            remove_tags,
+            likes
         ),
         output_processor=TakeFirst()
     )
