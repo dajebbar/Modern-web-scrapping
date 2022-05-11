3ec7bf9e2e825a1667e235b51def8226ec91fb60
Dajebbar <dajebbar@gmx.fr>
2022-05-11 10:10:15 +0100

    load loader

--- projects/goodreads/goodreads/spiders/quote.py
+++ projects/goodreads/goodreads/spiders/quote.py
@@ -13,13 +13,22 @@ class QuoteSpider(scrapy.Spider):
     def parse(self, response):
         quotes = response.xpath("//div[@class='quoteDetails']")
         for quote in quotes:
-            yield {
-                'author_img': quote.xpath(".//a[@class='leftAlignedImage']/img/@src").get(),
-                'text': quote.xpath("normalize-space(.//div[@class='quoteText']/text())").get(),
-                'author_name': quote.xpath("normalize-space(.//span[@class='authorOrTitle']/text())").get(),
-                'tags': quote.xpath("//div[@class='greyText smallText left']/a/text()").getall(),
-                'likes': quote.xpath("//a[@class='smallText']/text()").get(),
-            }
+            loader = ItemLoader(item=GoodreadsItem(), selector=quote, response=response)
+            loader.add_xpath("author_img", ".//a[@class='leftAlignedImage']/img/@src")
+            loader.add_xpath("text", "normalize-space(.//div[@class='quoteText'])")
+            loader.add_xpath("author_name", "normalize-space(.//span[@class='authorOrTitle'])")
+            loader.add_xpath("tags", ".//div[@class='greyText smallText left']/a")
+            loader.add_xpath("likes", ".//a[@class='smallText']")
+
+            yield loader.load_item()
+
+            # yield {
+            #     'author_img': quote.xpath(".//a[@class='leftAlignedImage']/img/@src").get(),
+            #     'text': quote.xpath("normalize-space(.//div[@class='quoteText']/text())").get(),
+            #     'author_name': quote.xpath("normalize-space(.//span[@class='authorOrTitle']/text())").get(),
+            #     'tags': quote.xpath(".//div[@class='greyText smallText left']/a/text()").getall(),
+            #     'likes': quote.xpath(".//a[@class='smallText']/text()").get(),
+            # }
 
         next_page = response.xpath("//a[@rel='next']/text()").get()
         if next_page:
