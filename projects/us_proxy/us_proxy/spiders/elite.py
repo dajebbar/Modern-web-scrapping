374531df18d172741cfc4f4b78dede87344b4ba9
Dajebbar <dajebbar@gmx.fr>
2022-05-12 10:44:21 +0100

    get proxies in columns

--- projects/us_proxy/us_proxy/spiders/elite.py
+++ projects/us_proxy/us_proxy/spiders/elite.py
@@ -4,7 +4,12 @@ import scrapy
 class EliteSpider(scrapy.Spider):
     name = 'elite'
     allowed_domains = ['hidemy.name']
-    start_urls = ['http://hidemy.name/']
+    start_urls = ['https://hidemy.name/en/proxy-list']
 
     def parse(self, response):
-        pass
+        table = response.xpath("//div[@class='table_block']/table")
+        rows = table.xpath(".//tbody/tr")
+        cols = [row.xpath(".//td/text()").getall() for row in rows]
+
+        print(cols)
+
